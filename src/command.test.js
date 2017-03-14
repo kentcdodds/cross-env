c1a9ed0764fe1e88f2ba370b43c4ade21d536f60
Hugo Wood <hx4.5@free.fr>
2017-03-14 17:22:22 +0100

    fix(args): make args conversion stateless (#85)
    * fix(args): make args conversion stateless
    
    * docs(args): replace short url by full url
    
    * docs(contributors): add @hgwood

--- src/command.test.js
+++ src/command.test.js
@@ -13,12 +13,20 @@ test(`converts unix-style env variable usage for windows`, () => {
 test(`leaves command unchanged when not a variable`, () => {
   expect(commandConvert('test')).toBe('test')
 })
+
 test(`converts windows-style env variable usage for linux`, () => {
   isWindowsMock.__mock.returnValue = false
   expect(commandConvert('%test%')).toBe('$test')
 })
 
 test(`leaves variable unchanged when using correct operating system`, () => {
-  isWindowsMock.__mock.returnValue = true
+  isWindowsMock.__mock.returnValue = false
   expect(commandConvert('$test')).toBe('$test')
+})
+
+test(`is stateless`, () => {
+  // this test prevents falling into regexp traps like this:
+  // http://stackoverflow.com/a/1520853/971592
+  isWindowsMock.__mock.returnValue = true
+  expect(commandConvert('$test')).toBe(commandConvert('$test'))
 })
