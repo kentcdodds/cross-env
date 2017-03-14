3ddf8461f514db6710308e29c7edb20078e0b3b6
Hugo Wood <hugo.wood-ext@vif.fr>
2017-03-14 18:22:41 +0100

    refactor(args): make commandConvert definitly stateless

--- src/command.js
+++ src/command.js
@@ -2,15 +2,14 @@ import isWindows from 'is-windows'
 
 export default commandConvert
 
-const envUseUnixRegex = /\$(\w+)|\${(\w+)}/g // $my_var or ${my_var}
-const envUseWinRegex = /%(.*?)%/g // %my_var%
-
 /**
  * Converts an environment variable usage to be appropriate for the current OS
  * @param {String} command Command to convert
  * @returns {String} Converted command
  */
 function commandConvert(command) {
+  const envUseUnixRegex = /\$(\w+)|\${(\w+)}/g // $my_var or ${my_var}
+  const envUseWinRegex = /%(.*?)%/g // %my_var%
   const isWin = isWindows()
   const envExtract = isWin ? envUseUnixRegex : envUseWinRegex
   return command.replace(envExtract, isWin ? '%$1$2%' : '$$$1')
