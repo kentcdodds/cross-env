e8a1614683fad01b2927ac8a4be308a21aa7df98
Daniel Rey López <daniel.rey.lopez@gmail.com>
2017-03-27 02:49:07 +0100

    fix: Resolve value of env variables before invoking cross-spawn (#95)
    * fix: Resolve value of env variables before invoking cross-spawn
    
    #90
    
    * Refactored the main parsing loop
    
    BREAKING CHANGE: This is unlikely to break anyone, but now if you assign a variable to a variable (like `FOO=$BAR` with the value `$BAR` being assigned to `hello`, the command will be converted to `FOO=hello` whereas before it was `FOO=$BAR`).

--- src/variable.js
+++ src/variable.js
@@ -1,16 +1,15 @@
 import isWindows from 'is-windows'
 
 /**
- * Converts an environment variable value to be appropriate for the current OS.
- * It will transform UNIX-style list values to Windows-style.
+ * This will transform UNIX-style list values to Windows-style.
  * For example, the value of the $PATH variable "/usr/bin:/usr/local/bin:."
  * will become "/usr/bin;/usr/local/bin;." on Windows.
- * @param {String} originalValue Original value of the env variable
+ * @param {String} varValue Original value of the env variable
  * @returns {String} Converted value
  */
-export default function varValueConvert(originalValue) {
+function replaceListDelimiters(varValue) {
   const targetSeparator = isWindows() ? ';' : ':'
-  return originalValue.replace(/(\\*):/g, (match, backslashes) => {
+  return varValue.replace(/(\\*):/g, (match, backslashes) => {
     if (backslashes.length % 2) {
       // Odd number of backslashes preceding it means it's escaped,
       // remove 1 backslash and return the rest as-is
@@ -18,4 +17,33 @@ export default function varValueConvert(originalValue) {
     }
     return backslashes + targetSeparator
   })
+}
+
+/**
+ * This will attempt to resolve the value of any env variables that are inside
+ * this string. For example, it will transform this:
+ * cross-env FOO=$NODE_ENV echo $FOO
+ * Into this:
+ * FOO=development echo $FOO
+ * (Or whatever value the variable NODE_ENV has)
+ * Note that this function is only called with the right-side portion of the
+ * env var assignment, so in that example, this function would transform
+ * the string "$NODE_ENV" into "development"
+ * @param {String} varValue Original value of the env variable
+ * @returns {String} Converted value
+ */
+function resolveEnvVars(varValue) {
+  const envUnixRegex = /\$(\w+)|\${(\w+)}/g // $my_var or ${my_var}
+  return varValue.replace(envUnixRegex, (_, varName, altVarName) => {
+    return process.env[varName || altVarName] || ''
+  })
+}
+
+/**
+ * Converts an environment variable value to be appropriate for the current OS.
+ * @param {String} originalValue Original value of the env variable
+ * @returns {String} Converted value
+ */
+export default function varValueConvert(originalValue) {
+  return resolveEnvVars(replaceListDelimiters(originalValue))
 }
